34eaef7b9fc3037b4c26822eeb90222e79fc17ab
제임스 <junwon.lee@majormap.net>
2023-07-18 16:31:04 +0900

    BaseModelService 설계변경 오류 수정

--- services.py
+++ services.py
@@ -1,4 +1,4 @@
-from typing import TypeVar, Generic
+from typing import TypeVar, Generic, get_args
 from pydantic import BaseModel as BaseSchema
 from sqlalchemy.orm import DeclarativeBase as BaseModel
 from sqlalchemy.orm.session import Session
@@ -22,6 +22,10 @@ class BaseModelService(
         UpdatePayloadT,
     ]
 ):
+    _model_class: type[ModelT]
+    _list_item_schema: type[ListItemSchemaT]
+    _detail_schema: type[DetailSchemaT]
+
     db: Session
     autocommit: bool = False
 
@@ -29,18 +33,21 @@ class BaseModelService(
         self.db = db
         self.autocommit = autocommit
 
+        self._model_class = get_args(self.__class__.__orig_bases__[0])[0]
+        self._list_item_schema = get_args(self.__class__.__orig_bases__[0])[1]
+        self._detail_schema = get_args(self.__class__.__orig_bases__[0])[2]
+
     def get_instance(self, detail_id: int, raises_exc=True) -> ModelT:
         from sqlalchemy.sql.expression import select
 
-        stmt_0 = select(type[ModelT]).filter_by(id=detail_id)
-
+        stmt_0 = select(self._model_class).filter_by(id=detail_id)
         if raises_exc:
             return self.db.execute(stmt_0).scalar_one()
         else:
             return self.db.execute(stmt_0).scalar_one_or_none()
 
     def get_list_item_from_instance(self, instance: ModelT) -> ListItemSchemaT:
-        return type[ListItemSchemaT].from_orm(instance)
+        return self._list_item_schema.from_orm(instance)
 
     def get_paginated_list(
         self, stmt: ClauseElement, page: int = 0, per_page: int = 20
@@ -67,7 +74,7 @@ class BaseModelService(
         )
 
     def get_detail_from_instance(self, instance: ModelT) -> DetailSchemaT:
-        return type[DetailSchemaT].from_orm(instance)
+        return self._detail_schema.from_orm(instance)
 
     def get_detail(self, detail_id: int) -> DetailSchemaT:
         instance = self.get_instance(detail_id)
@@ -77,7 +84,7 @@ class BaseModelService(
         return instance
 
     def create(self, payload: CreationPayloadT) -> ModelT:
-        instance = type[ModelT](**payload.dict())
+        instance = self._model_class(**payload.dict())
         self.db.add(instance)
         self.create_post_hook(instance)
 
@@ -108,7 +115,7 @@ class BaseModelService(
     def delete(self, detail_id: int) -> None:
         from sqlalchemy.sql.expression import delete
 
-        stmt_0 = delete(type[ModelT]).filter_by(id=detail_id)
+        stmt_0 = delete(self._model_class).filter_by(id=detail_id)
         self.db.execute(stmt_0)
 
         if self.autocommit:
