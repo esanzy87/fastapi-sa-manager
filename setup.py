58b89d59a1763eab7ec92a50148d5366a1c61de1
제임스 <junwon.lee@majormap.net>
2023-07-31 17:32:32 +0900

    bug fix: directory structure for import

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fastapi-sa-manager",
-    version="0.1.1",
+    version="0.1.2",
     description="FastAPI-sa-manager is an SQLAlchemy ORM extension for FastAPI helping you to build CRUD APIs fast and easy.",
     author="esanzy87",
     author_email="esanzy87@gmail.com",
